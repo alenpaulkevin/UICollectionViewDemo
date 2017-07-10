71fd9b6188764554a395d94df24cbfcbea4692fd
meidaeriOS <alenpaulkevin@163.com>
2017-07-10 15:00:20 +0800

    修改

--- UICollectionViewDemo/Pods/Kingfisher/Sources/AnimatedImageView.swift
+++ UICollectionViewDemo/Pods/Kingfisher/Sources/AnimatedImageView.swift
@@ -179,25 +179,7 @@ open class AnimatedImageView: UIImageView {
     
     /// Update the current frame with the displayLink duration.
     private func updateFrame() {
-        let duration: CFTimeInterval
-
-        // CA based display link is opt-out from ProMotion by default.
-        // So the duration and its FPS might not match. 
-        // See [#718](https://github.com/onevcat/Kingfisher/issues/718)
-        if #available(iOS 10.0, tvOS 10.0, *) {
-            // By setting CADisableMinimumFrameDuration to YES in Info.plist may 
-            // cause the preferredFramesPerSecond being 0
-            if displayLink.preferredFramesPerSecond == 0 {
-                duration = displayLink.duration
-            } else {
-                // Some devices (like iPad Pro 10.5) will have a different FPS.
-                duration = 1.0 / Double(displayLink.preferredFramesPerSecond)
-            }
-        } else {
-            duration = displayLink.duration
-        }
-    
-        if animator?.updateCurrentFrame(duration: duration) ?? false {
+        if animator?.updateCurrentFrame(duration: displayLink.duration) ?? false {
             layer.setNeedsDisplay()
         }
     }
@@ -226,7 +208,7 @@ class Animator {
     fileprivate var timeSinceLastFrameChange: TimeInterval = 0.0
     fileprivate var needsPrescaling = true
     
-    /// Loop count of animated image.
+    /// Loop count of animatd image.
     private var loopCount = 0
     
     var currentFrame: UIImage? {
---
+++ UICollectionViewDemo/Pods/Target Support Files/Pods-UICollectionViewDemo/Pods-UICollectionViewDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UICollectionViewDemo : NSObject
+@end
+@implementation PodsDummy_Pods_UICollectionViewDemo
+@end
